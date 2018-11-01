07710e803d5778020dc2eb045a31936029d00410
Justin Kenney <justinkenney13@gmail.com>
2018-11-01 14:59:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,22 +5,34 @@ class Student
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
-    @id = id    
+    @id = id
   end
 
   def self.create_table
     sql = <<-SQL
-    "CREATE TABLE IF NONE EXISTS table(
+    "CREATE TABLE IF NONE EXISTS students(
     id INTEGER PRIMARY KEY,
     name TEXT,
-    grade TEXT 
+    grade TEXT
     )"
     SQL
 
-    DB[:conn].execute(sql)    
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    sql = <<-SQL
+    "INSERT INTO students (name, grade) VALUES (?, ?)"
+    SQL
+
+    DB[:conn].execute(sql)(self.name, self.grade)
   end
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
